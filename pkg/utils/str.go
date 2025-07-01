2974b0d1be64560e8134d14ab1d8a5df9734e520
qiankunli <qiankunli@qq.com>
2025-07-01 16:48:48 +0800

    feat: support multi addr seperator

--- pkg/utils/str.go
+++ pkg/utils/str.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"regexp"
+	"strings"
+)
+
 func FirstNonEmpty(str1, str2 string) string {
 	if str1 != "" {
 		return str1
@@ -8,4 +13,10 @@ func FirstNonEmpty(str1, str2 string) string {
 		return str2
 	}
 	return ""
+}
+
+func SplitByMultipleDelimiters(s string, delimiters ...string) []string {
+	delimiterPattern := "[" + regexp.QuoteMeta(strings.Join(delimiters, "")) + "]"
+	re := regexp.MustCompile(delimiterPattern)
+	return re.Split(s, -1)
 }
